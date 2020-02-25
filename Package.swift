ddef186c238a9a831fb4404b54b1bebf33082d73
Manikanta Nomula <c-manikanta.nomula@charter.com>
2020-02-25 10:23:15 -0700

    Added a hard dependency on the 0.0.3 version of Protocols Framework.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/popmedic/decouple-protocols.git", from:"0.0.3"
+            url: "https://github.com/popmedic/decouple-protocols.git", "0.0.3"..."0.0.3"
         ),
     ],
     targets: [
